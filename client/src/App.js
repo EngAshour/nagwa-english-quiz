5f32546f72e662c11bfd257825ae409d2d784f8f
EngAshour <101207081+EngAshour@users.noreply.github.com>
2022-07-25 23:04:02 +0200

    Update the App.js
    Using the useEffect and useState to create the fetch of the json data and future needed futures.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function App() {}
+function App() {
+    const [backendWord, setBackendWord] = useState([
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+    ]);
+    const [backendRank, setBackendRank] = useState([{}]);
+    const [showFinalResults, setFinalResults] = useState(false);
+    const [score, setScore] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    useEffect(() => {
+        fetch(`/words`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                return response.json();
+            })
+            .then((actualData) => setBackendWord(actualData))
+            .catch((err) => {
+                console.log(err.message);
+            });
+        fetch(`/rank`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                return response.json();
+            })
+            .then((actualData) => setBackendRank(actualData))
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+}
 
 export default App;
