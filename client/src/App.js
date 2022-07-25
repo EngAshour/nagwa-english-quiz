3feff60b0283d6189d1d1724fd8f81d5db3765c4
EngAshour <101207081+EngAshour@users.noreply.github.com>
2022-07-25 23:06:03 +0200

    Helper Functions.
    Create the answer clic, restart and the shuffle function that gonna need in side the application.

--- client/src/App.js
+++ client/src/App.js
@@ -45,6 +45,42 @@ function App() {
                 console.log(err.message);
             });
     }, []);
+
+    // Helper Functions:
+    const ansClicked = (pos, value) => {
+        if (backendWord[currentQuestion].pos === value) {
+            setScore(score + 1);
+        }
+        if (currentQuestion + 1 < 10) {
+            setCurrentQuestion(currentQuestion + 1);
+        } else {
+            setFinalResults(true);
+        }
+        console.log(score);
+    };
+
+    const restartQuiz = () => {
+        setScore(0);
+        setCurrentQuestion(0);
+        setFinalResults(false);
+    };
+
+    function shuffle(array) {
+        let currentIndex = array.length,
+            randomIndex;
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
+        }
+        return array;
+    }
 }
 
 export default App;
