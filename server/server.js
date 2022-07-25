f2ef2f7780b47a8395f3df43b33541c6270dc429
EngAshour <101207081+EngAshour@users.noreply.github.com>
2022-07-25 23:01:35 +0200

    Adding the JSON File and start the server
    creating the endpoints in the server and make listen at port 5000.and remove the unnecessary files created by React app.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
+const data = require("./TestData.json");
 
-app.get("/api", (req, res) => {});
+app.get("/words", (req, res) => {
+    res.json(data.wordList);
+});
+
+app.get("/rank", (req, res) => {
+    res.json(data.scoresList);
+});
+
+app.listen(5000, () => {
+    console.log("Server started on Port 5000");
+});
